c0845988231e0830514372de4a55654c8c478934
Dan Albert <danalbert@google.com>
2015-12-11 10:38:49 -0800

    Add support for building with NDK r11.
    NDK r11 is not released yet, but this allows building with the
    pre-release package.

--- build-android.sh
+++ build-android.sh
@@ -262,6 +262,11 @@ case "$NDK_RN" in
 		CXXPATH=$AndroidNDKRoot/toolchains/${TOOLCHAIN}/prebuilt/${PlatformOS}-x86_64/bin/arm-linux-androideabi-g++
 		TOOLSET=gcc-androidR8e
 		;;
+	"11")
+		TOOLCHAIN=${TOOLCHAIN:-arm-linux-androideabi-4.9}
+		CXXPATH=$AndroidNDKRoot/toolchains/${TOOLCHAIN}/bin/arm-linux-androideabi-g++
+		TOOLSET=gcc-androidR11
+		;;
 	*)
 		echo "Undefined or not supported Android NDK version!"
 		exit 1
